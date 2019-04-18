84b5562885a3a011ac284a0ef5ff53d286ff5cd7
hj <hujingleo01@163.com>
2019-04-18 14:16:51 +0800

    将项目打包方式改为war

--- src/main/java/com/cikers/wechat/mall/CikersApplication.java
+++ src/main/java/com/cikers/wechat/mall/CikersApplication.java
@@ -2,12 +2,18 @@ package com.cikers.wechat.mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CikersApplication {
+public class CikersApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CikersApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CikersApplication.class);
     }
 
 }
